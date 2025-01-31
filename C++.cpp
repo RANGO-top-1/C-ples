d906416cdd1ba5f23374aafcf8b32e1fb185cd08
RANGO-top-1 <artur.detistov@bk.ru>
2025-01-31 18:18:24 +0300

    C++1

---
+++ C++.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double x;
+    double y;
+    cout << "Введите x: ";
+    cin >> x; 
+    cout << "Введите y: ";
+    cin >> y;
+   
+    double  d = x * pow(y, 2) - sqrt(pow(x, 2) - 2.5 * pow(10, -3) * y) / (2 * sin(x * y)) + 0.5;
+    cout << endl << " Ответ " << d << endl ;
+
