2086b21d3c53ebd82c182663adff7596d527c114
RANGO <artur.detistov@bk.ru>
2025-10-23 11:19:16 +0300

    Add files via upload

---
+++ masivVosrastanie.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a [] = {2, 26, 1, 6, 5, 10, 11, 9, 29};
+    vector <int> result;
+    int primi = a[0];
+    result.push_back(primi);
+
+	for (int i = 0; i < 10; i++) {
+        if (primi < a[i]) {
+            primi = a[i];
+            result.push_back(primi);
+        }
+        else (primi < a[i]); {
+            continue;
+		}
+	}
+    for (int end : result) {
+        cout << end << " ";
+    }
+}
