90b7a5f92a5ced6fe4031158bec3342a92b22d92
RANGO <artur.detistov@bk.ru>
2025-10-23 11:21:34 +0300

    Add files via upload

---
+++ masiv_plus.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int arr[] = { 9, 5, 97, 9, 2, -6, 4, -5, 68, 68, 1 };
+    
+    int start = 0;
+    int end = 0;
+
+    int arrMin = arr[0];
+
+    for (int i = 1; i < arr[0]; i++) {
+        if (arr[i] < arrMin) {
+            start = i;
+            arrMin = arr[i];
+        }
+    }
+
+    int arrMax = arr[0];
+
+    for (int i = 1; i < arr[0]; i++) {
+        if (arr[i] > arrMax) {
+            end = i;
+            arrMax = arr[i];
+        }
+    }
+
+    if (start > end) {
+        int c = start;
+        start = end;
+        end = c;
+    }
+
+    int sum = 0;
+    for (int i = start + 1; i < end; i++) {
+        sum += arr[i];
+    }
+
+
+    cout << "Min:" << arrMin << endl;
+    cout << "Max:" << arrMax << endl;
+    cout << "Масив:  {9, 5, 97, 9, 2, -6, 4, -5, 68, 68, 1}" <<  endl;
+    cout << "Сумма:" << sum << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
